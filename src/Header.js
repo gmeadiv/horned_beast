52e5dbe07ed516263e023b477642aac44289a9cb
George Mead <gmeadiv@gmail.com>
2021-09-13 14:26:57 -0700

    fixed typos

--- src/Header.js
+++ src/Header.js
@@ -2,7 +2,7 @@ import {Component} from 'react';
 
 class Header extends Component {
   render() {
-    return <h1>Title</h1>
+    return <h1>The Horned Beasts of Springfield</h1>
   }
 }
 
--- src/Main.js
+++ src/Main.js
@@ -6,9 +6,8 @@ class Main extends Component {
   render() {
     return (
       <>
-        <h1>Horned Beasts</h1>
-        <HornedBeast title={'Itchy'} imgURL={itchy} description={'A fierce beast whose strength and spirit makes up for its size'} />
-        <HornedBeast title={'Scratchy'} imgURL={scratchy} description={'Not the brightest beast but is the biggest.'} />
+        <HornedBeast title={'Itchy'} imgURL={itchy} description={'A fierce beast whose strength and spirit makes up for its size.'} />
+        <HornedBeast title={'Scratchy'} imgURL={scratchy} description={'Not the brightest, but is the biggest.'} />
       </>
     )
   }
@@ -19,7 +18,7 @@ class HornedBeast extends Component {
     return (
     <>
       <h2>This horned beast is known as {this.props.title}</h2>
-      <img src={this.props.imgURL} alt='hornedBeast_img'/>
+      <img src={this.props.imgURL} alt='hornedBeast_img' title={this.props.title} />
       <p>{this.props.description}</p>
     </>
     )
